a9230253fcab60aeedc9d187dab6d827ae037f58
Ryusei Wada <wada@emol.jp>
2021-12-14 17:48:30 +0900

    Initial Commit

---
+++ GitRestApiApp/GitRestApiAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  GitRestApiAppApp.swift
+//  GitRestApiApp
+//
+//  Created by Ryusei Wada on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct GitRestApiAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
